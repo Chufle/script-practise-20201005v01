e6e063387673e504d893d8a06b48c9d79d5c323d
“Daniel <daniel.schoenfeldt@gmx.de>
2022-10-05 12:21:36 +0200

    script01.sh asking for city

--- script01.sh
+++ script01.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 echo "Hello, what is your name?"
 read name
+echo "In which city are you living?"
+read city
 mkdir $name
 echo "Userdata for $name" >> $name/userdata_$name.txt
-echo "Hey $name, I've created a folder with your name and a txt file in it"
+echo "$name lives in $city" >> $name/userdata_$name.txt
+echo "Hey $name, I've created a folder with your name and a data txt file in it"
