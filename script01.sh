c2ef59ebcd07335972a588744d164b7667869930
“Daniel <daniel.schoenfeldt@gmx.de>
2022-10-05 12:13:14 +0200

    script01.sh added name in txt

--- script01.sh
+++ script01.sh
@@ -2,5 +2,5 @@
 echo "Hello, what is your name?"
 read name
 mkdir $name
-echo "Userdata for $name" >> $name/userdata.txt
+echo "Userdata for $name" >> $name/userdata_$name.txt
 echo "Hey $name, I've created a folder with your name and a txt file in it"
